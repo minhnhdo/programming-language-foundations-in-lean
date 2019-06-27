6582160c7370997f73a135a7e9f05af952556677
Do Nhat Minh <minhnhdo@gmail.com>
2019-06-27 08:23:04 -0700

    switched to monad methods

--- src/type_checking.lean
+++ src/type_checking.lean
@@ -7,30 +7,30 @@ def type_check : context -> tm -> option ty
   return (ty.arrow T T')
 | gamma (tm.app t₁ t₂) :=
   match type_check gamma t₁, type_check gamma t₂ with
-  | (some (ty.arrow T T')), (some T₂) := if T = T₂ then some T' else none
-  | _, _ := none
+  | (some (ty.arrow T T')), (some T₂) := if T = T₂ then return T' else failure
+  | _, _ := failure
   end
-| _ (tm.const _) := some ty.nat
+| _ (tm.const _) := return ty.nat
 | gamma (tm.prd t) := do
-  ty.nat <- type_check gamma t | none,
+  ty.nat <- type_check gamma t | failure,
   return ty.nat
 | gamma (tm.scc t) := do
-  ty.nat <- type_check gamma t | none,
+  ty.nat <- type_check gamma t | failure,
   return ty.nat
 | gamma (tm.mlt t₁ t₂) := do
-  ty.nat <- type_check gamma t₁ | none,
-  ty.nat <- type_check gamma t₂ | none,
+  ty.nat <- type_check gamma t₁ | failure,
+  ty.nat <- type_check gamma t₂ | failure,
   return ty.nat
 | gamma (tm.iszro t) := do
-  ty.nat <- type_check gamma t | none,
+  ty.nat <- type_check gamma t | failure,
   return ty.bool
-| _ tm.tru := some ty.bool
-| _ tm.fls := some ty.bool
+| _ tm.tru := return ty.bool
+| _ tm.fls := return ty.bool
 | gamma (tm.tst t t₁ t₂) := do
-  ty.bool <- type_check gamma t | none,
+  ty.bool <- type_check gamma t | failure,
   T <- type_check gamma t₁,
   T' <- type_check gamma t₁,
-  if T = T' then return T else none
+  if T = T' then return T else failure
 | gamma (tm.let_ x t₁ t₂) := do
   T₁ <- type_check gamma t₁,
   T₂ <- type_check (partial_map.update x T₁ gamma) t₂,
@@ -40,12 +40,12 @@ def type_check : context -> tm -> option ty
   T₂ <- type_check gamma t₂,
   return (ty.prod T₁ T₂)
 | gamma (tm.fst t) := do
-  (ty.prod T₁ _) <- type_check gamma t | none,
+  (ty.prod T₁ _) <- type_check gamma t | failure,
   return T₁
 | gamma (tm.snd t) := do
-  (ty.prod _ T₂) <- type_check gamma t | none,
+  (ty.prod _ T₂) <- type_check gamma t | failure,
   return T₂
-| _ tm.unit := some ty.unit
+| _ tm.unit := return ty.unit
 | gamma (tm.inl T₂ t) := do
   T₁ <- type_check gamma t,
   return (ty.sum T₁ T₂)
@@ -53,25 +53,25 @@ def type_check : context -> tm -> option ty
   T₂ <- type_check gamma t,
   return (ty.sum T₁ T₂)
 | gamma (tm.scase t x t₁ y t₂) := do
-  (ty.sum T₁ T₂) <- type_check gamma t | none,
+  (ty.sum T₁ T₂) <- type_check gamma t | failure,
   T <- type_check (partial_map.update x T₁ gamma) t₁,
   T' <- type_check (partial_map.update y T₂ gamma) t₂,
-  if T = T' then some T else none
-| _ (tm.nil T) := some (ty.list T)
+  if T = T' then return T else failure
+| _ (tm.nil T) := return (ty.list T)
 | gamma (tm.cons t₁ t₂) := do
   T <- type_check gamma t₁,
-  (ty.list T') <- type_check gamma t₂ | none,
-  if T = T' then some (ty.list T) else none
+  (ty.list T') <- type_check gamma t₂ | failure,
+  if T = T' then return (ty.list T) else failure
 | gamma (tm.lcase t t₁ y z t₂) := do
-  (ty.list T) <- type_check gamma t | none,
+  (ty.list T) <- type_check gamma t | failure,
   T' <- type_check gamma t₁,
   T'' <- type_check (partial_map.update z (ty.list T) $
                      partial_map.update y T gamma)
                     t₂,
-  if T' = T'' then some T' else none
+  if T' = T'' then return T' else failure
 | gamma (tm.fix t) := do
-  (ty.arrow T T') <- type_check gamma t | none,
-  if T = T' then some T else none
+  (ty.arrow T T') <- type_check gamma t | failure,
+  if T = T' then return T else failure
 
 theorem type_checking_complete {gamma t T} (ht : has_type gamma t T) :
   type_check gamma t = some T :=
